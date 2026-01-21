fdeaa7990cc6a79f389e735cfaaa66b045e84779
TARIQ ALJAARAT <168788587+p5hzehxa@users.noreply.github.com>
2026-01-22 01:23:07 +0300

    Initialize upgrade-gulp

---
+++ src/lib/validation/formValidators/user.ts
@@ -0,0 +1,45 @@
+import logger from "./../../Logger";
+import { GenericValidator } from "./../../validation/generic";
+
+export class UserFormsValidator extends GenericValidator {
+    constructor () {
+        super();
+    }
+
+    validateSettingsUpdate (payload: any): Promise<any> {
+        logger.info(`Request to validate update user settings payload`);
+
+        try {
+            if (typeof payload.email !== "undefined" && !payload.email.length) {
+                this.errors.stack.email = this.errorManifest.validation.email.blank;
+            } else if (!this.isValidEmail(payload.email)) {
+                this.errors.stack.email = this.errorManifest.validation.email.incorrect;
+            }
+
+            // validate additional fields in payload here, adding to error object as and when validation fails
+
+            // finally, check if all fields validated correctly, or if one or more of them failed
+            if (!Object.keys(this.errors.stack).length) {
+                return Promise.resolve(true);
+            } else {
+                return Promise.reject(this.errors);
+            }
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                logger.error(err.message);
+            }
+            this.errors.stack = this.errorManifest.generic.serverError;
+            return Promise.reject(this.errors);
+        }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    validateProfileUpdate (payload: any): Promise<any> {
+        return Promise.resolve(true);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    validateRegistration (payload: any): Promise<any> {
+        return Promise.resolve(true);
+    }
+};
---
+++ src/routers/handlers/company/details.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { GenericHandler } from "./../generic";
+import logger from "../../../lib/Logger";
+
+export class DetailsHandler extends GenericHandler {
+    constructor () {
+        super();
+        this.viewData.title = "Details handler for company router";
+        this.viewData.sampleKey = "sample value for company details screen";
+    }
+
+    execute (req: Request, response: Response, method: string = "GET"): Promise<object> {
+        logger.info(`${method} request for to get company details, with id: ${req.params.id}`);
+        // ...process request here and return data for the view
+        return Promise.resolve(this.viewData);
+    }
+
+    // additional support method in handler
+    private supportMethod1 (): boolean {
+        return true;
+    }
+
+    // additional support method in handler
+    private supportMethod2 (): boolean {
+        return false;
+    }
+};
